24375bc53bad08e998189d8a9e14f73aa464397a
DESKTOP-HJL050Q\ez220409 <moonx297@crk.umn.edu>
2022-07-12 18:02:40 +0900

    JSP MVC2 회원관리

---
+++ pro17/src/main/java/sec01/ex01/MemberDAO.java
@@ -0,0 +1,77 @@
+package sec01.ex01;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	private DataSource dataFactory;
+	private Connection conn;
+	private PreparedStatement pstmt;
+
+	public MemberDAO() {
+		try {	//컨넥션 풀. 연결 하는 기능 servers -> context.xml과 매칭시키기
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<MemberVO> listMembers() {
+		List<MemberVO> membersList = new ArrayList();
+		try {
+			conn = dataFactory.getConnection();
+			String query = "select * from  t_member order by joinDate desc";
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery(); //조회만 하겠다. -> select와 짝꿍. 만약 insert이면 executeUpdate. 
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				MemberVO memberVO = new MemberVO(id, pwd, name, email, joinDate);
+				membersList.add(memberVO);
+			}
+			rs.close();
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return membersList;
+	}
+
+	public void addMember(MemberVO m) {
+		try {
+			conn = dataFactory.getConnection();
+			String id = m.getId();
+			String pwd = m.getPwd();
+			String name = m.getName();
+			String email = m.getEmail();
+			String query = "INSERT INTO t_member(id, pwd, name, email)" + " VALUES(?, ? ,? ,?)";	//회원가입. insert -> executeUpdate
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			pstmt.setString(3, name);
+			pstmt.setString(4, email);
+			pstmt.executeUpdate();
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
