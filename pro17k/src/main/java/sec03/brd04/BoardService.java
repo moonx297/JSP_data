d159ff2866b16202f71a15a2d1a2766216d40279
DESKTOP-HJL050Q\ez220409 <moonx297@crk.umn.edu>
2022-07-15 17:10:09 +0900

    MVC2 게시판 답글 071522

---
+++ pro17k/src/main/java/sec03/brd04/BoardService.java
@@ -0,0 +1,33 @@
+package sec03.brd04;
+
+import java.util.List;
+
+
+
+public class BoardService {
+	BoardDAO boardDAO;
+	
+	public BoardService()
+	{
+		boardDAO = new BoardDAO();
+	}
+	
+	public int addArticle(ArticleVO article) {
+		return boardDAO.insertNewArticle(article);
+	}
+	
+	public List<ArticleVO> listArticles()
+	{
+		List<ArticleVO> articlesList = boardDAO.selectAllArticles();
+		return articlesList;
+	}
+}
+	public ArticleVO viewArticle(int articleNO)
+	{
+		ArticleVO article = null;
+		article = boardDAO.selectArticle(articleNO);
+		return article;
+	}
+
+
+	
